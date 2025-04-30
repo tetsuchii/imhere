5f993a4bcbfa64ee1148978ecc4e71089170529c
tetsuchii <panka.toro@gmail.com>
2025-04-30 12:00:41 +0200

    veronika on board too

--- shake-sender/App.js
+++ shake-sender/App.js
@@ -2,11 +2,25 @@ import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { DeviceMotion } from "expo-sensors";
 import * as Ably from "ably";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+let deviceId;
 const ably = new Ably.Realtime(
   "DKaEwg.3nk-tg:bJ_kgD9qWySagyTuNq_mqJMLSaQTEb5n8E_tZ8Fytso"
 ); // we’ll fill this soon
 const channel = ably.channels.get("shake-channel");
+
+const loadDeviceId = async () => {
+  const storedId = await AsyncStorage.getItem("device-id");
+  if (storedId) {
+    deviceId = storedId;
+  } else {
+    deviceId = uuidv4();
+    await AsyncStorage.setItem("device-id", deviceId);
+  }
+};
 
 export default function App() {
   useEffect(() => {
@@ -18,7 +32,7 @@ export default function App() {
 
       if (totalForce > 25 && Date.now() - lastShake > 1000) {
         lastShake = Date.now();
-        channel.publish("shake", { emoji: "📳" });
+        channel.publish("shake", { deviceId });
         console.log("Shake detected and sent!");
       }
     });
